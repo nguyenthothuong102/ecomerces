8b6f43f1207e418d7b54b07c5633d7fcbba2cb32
nguyenthothuong102 <thuongmonster.slna37@gmail.com>
2019-12-14 20:15:51 +0700

    init_project

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require jquery.min
+//= require bootstrap.min
+//= require slick.min
+//= require nouislider.min
+//= require jquery.zoom.min
+//= require main
